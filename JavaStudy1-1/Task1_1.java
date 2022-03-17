3fcaa604731595177dc17d9afbff96b1e609a284
Uaisara <100998976+Uaisara@users.noreply.github.com>
2022-03-17 19:50:57 +0900

    データ型・文字列クラス 提出課題
    JavaStudy1-1\Task1_1.java

---
+++ JavaStudy1-1/Task1_1.java
@@ -0,0 +1,13 @@
+public class Task1_1 {public static void main(String[] args) {
+    int hogeInt=100;
+    String hogeString="合格";
+    // doubleクラスのhogeDoubleという名前の変数を宣言、値「0.2」で初期化
+    double hogeDouble = 0.2;
+    System.out.print(hogeInt);
+    // hogeStringを表示
+    System.out.println(hogeString);
+    System.out.print(hogeDouble);
+
+
+}
+}
